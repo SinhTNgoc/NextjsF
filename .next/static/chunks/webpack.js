b349f611c706ea1d515613e8bf9a107141d3ece9
Tran Sinh <sinhtran@MacBook-Pro-2.local>
2021-09-27 09:14:39 +0700

    Test: FONT

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c05312bf4229ff0f2c1"; }
+/******/ 		__webpack_require__.h = function() { return "6003f1156e23e742f3c6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
